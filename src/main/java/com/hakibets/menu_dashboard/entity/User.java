03e64dc800e7d1b8f63dbda1b11a38dee877dd9f
veenjenga <vionajoynjenga@gmail.com>
2025-11-26 13:34:59 +0300

    Fixing authorization error

--- src/main/java/com/hakibets/menu_dashboard/entity/User.java
+++ src/main/java/com/hakibets/menu_dashboard/entity/User.java
@@ -4,14 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;   // ← NEW IMPORT
 
 @Entity
+@Table(name = "users")           // ← THIS LINE FIXES THE 500 ERROR
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        private String username;
-        private String password;
+    private Long id;
+    private String username;
+    private String password;
 
     public Long getId() {
         return id;
